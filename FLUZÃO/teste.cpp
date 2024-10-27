fc4152e31c0c1abb9cdf6189b8b8409ecbb193f0
murilopn <murilopatricionascimento@gmail.com>
2024-10-27 15:06:52 +0000

    testando ssh key

---
+++ FLUZÃO/teste.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main (){
+    
+printf("Sou tricolor de coração, sou do clube tantas vezes campeão fascina pela sua disciplina o fluminense me domina ");
+printf("eu tenho amor ao tricolor. Salva o primeiro pavilhão, das 3 cores que traduzem tradição\n");
+    
+    return 0;
+}
