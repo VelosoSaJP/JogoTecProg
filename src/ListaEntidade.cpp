bc9a44820a7e5d66487b0773b1d35649f80d9cfc
Joao Pedro Veloso <jpedrovelososa@gmail.com>
2024-12-09 21:35:53 -0300

    introdução das listas-NAO FINALIZADA

---
+++ src/ListaEntidade.cpp
@@ -0,0 +1,11 @@
+#include "../include/List/ListaEntidade.h"  
+
+namespace Lista{
+
+    ListaEntidade::ListaEntidade(){}
+
+    ListaEntidade::~ListaEntidade(){
+        
+    }
+
+}//namespace Lista
